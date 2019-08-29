9e227ce2bfd75551d8b9b1e668c2cef242e41bc9
cdk8s-harvestmoon <526557444@qq.com>
2019-08-29 23:18:09 +0800

    feat: 添加 prod 环境

--- src/constants/globalConstant.ts
+++ src/constants/globalConstant.ts
@@ -21,4 +21,14 @@ exports.constant = {
         [`${prefix}REQUEST_SPLICING_TKEY_SSO_BASE_URL`]: 'http://sso.cdk8s.com/sso/oauth/authorize?response_type=code&client_id=client_management&redirect_uri=',
         [`${prefix}REQUEST_SPLICING_TKEY_SSO_CODE_CALLBACK`]: 'http://sso.cdk8s.com/tkey-sso-client-management-frontend/codeCallback?redirect_uri=',
     },
+    prodConstant: {
+        [`${prefix}SYSTEM_TITLE_OPEN`]: 'TKey SSO Client 管理系统',
+        [`${prefix}SYSTEM_TITLE_CLOSE`]: 'TKey',
+        [`${prefix}LOCAL_STORAGE_TOKEN_KEY`]: 'tkey-token',
+        [`${prefix}REQUEST_HEADER_TOKEN_KEY`]: 'x-token',
+        [`${prefix}API_SERVER`]: 'http://sso.cdk8s.com/sso-client-management',
+        [`${prefix}LOGOUT_URL`]: 'http://sso.cdk8s.com/sso-client-management/logout?redirect_uri=http://sso.cdk8s.com/tkey-sso-client-management-frontend/TKeyClient',
+        [`${prefix}REQUEST_SPLICING_TKEY_SSO_BASE_URL`]: 'http://sso.cdk8s.com/sso/oauth/authorize?response_type=code&client_id=client_management&redirect_uri=',
+        [`${prefix}REQUEST_SPLICING_TKEY_SSO_CODE_CALLBACK`]: 'http://sso.cdk8s.com/tkey-sso-client-management-frontend/codeCallback?redirect_uri=',
+    },
 };
